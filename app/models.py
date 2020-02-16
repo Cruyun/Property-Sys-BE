3d76ffc580045cacdc905def5682be5c77d743dd
Cruyun <1045671796@qq.com>
2020-02-16 23:45:24 +0800

    add classes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-import time
+from datetime import datetime
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin,AnonymousUserMixin
@@ -8,7 +8,7 @@ from flask import current_app
 class User(UserMixin,db.Model):
   __tablename__='users'
   id=db.Column(db.Interger, primary_key=True)
-  username=db.Column(db.String(20), unique=False)
+  username=db.Column(db.String(20))
   password=db.Column(db.String(20))
   password_hash=db.Column(db.String(128))
   is_admin=db.Column(db.Boolean, default=False)
@@ -53,4 +53,67 @@ class User(UserMixin,db.Model):
   def load_user(user_id):
     return User.query.get(int(user_id))
 
+# 小区
+class Estate(db.Model):
+  __tablename__='estates'
+  id=db.Column(db.Interger, primary_key=True)
+  name=db.Column(db.String(20), unique=True)
+  resident_num=db.Column(db.Interger, default=0)
 
+# 住房
+class Room(db.Model):
+  __tablename__='rooms'
+  id=db.Column(db.Interger, primary_key=True)
+  name=db.Column(db.String(20))
+  water_used=db.Column(db.Interger, default=0)
+  water_fee=db.Column(db.Interger, default=0)
+  gas_used=db.Column(db.Interger, default=0)
+  gas_fee=db.Column(db.Interger, default=0)
+  ele_used=db.Column(db.Interger, default=0)
+  ele_fee=db.Column(db.Interger, default=0)
+  owner_id=db.Column(db.Interger, db.ForeignKey('users.id')) # 户主 id可为空
+  estate_id=db.Column(db.Interger, db.ForeignKey('estates.id'), nullable=False) # 小区id
+
+#报告
+class Report(db.Model):
+  __tablename__='reports'
+  id=db.Column(db.Interger, primary_key=True)
+  content=db.Column(db.String(255))
+  state=db.Column(db.Interger, default=0) #状态 0 未处理 1 正在处理 2 已处理
+  type=db.Column(db.Interger, default=0) #类型 0 维修 1 投诉 2 其他
+  submit_time=db.Column(db.DateTime, default=datetime.now)
+  solved_time=db.Column(db.DateTime)
+  user_id=db.Column(db.Interger, db.ForeignKey('users.id')) # 报告人 id
+  admin_id=db.Column(db.Interger, db.ForeignKey('users.id')) # 处理的管理员 id
+  estate_id=db.Column(db.Interger, db.ForeignKey('estates.id'), nullable=False)
+
+# 通知公告
+class Announcement(db.Model):
+  __tablename__='announcements'
+  id=db.Column(db.Interger, primary_key=True)
+  content=db.Column(db.String(1000))
+  time=db.Column(db.DateTime, default=datetime.now)
+  estate_id=db.Column(db.Interger, db.ForeignKey('estates.id'), nullable=False)
+
+# 停车场
+class Parking(db.Model):
+  __tablename__='parking'
+  id=db.Column(db.Interger, primary_key=True)
+  address=db.Column(db.String(20))
+  state=db.Column(db.Interger, default=0, nullable=False) #状态 0 可停车 1 不可停车
+  free_start=db.Column(db.DateTime) #车位主人可修改的开放游客停车时间
+  free_end=db.Column(db.DateTime)
+  owner_id=db.Column(db.Interger, db.ForeignKey('users.id')) # 车位主人id
+  estate_id=db.Column(db.Interger, db.ForeignKey('estates.id'), nullable=False)
+
+# 缴费
+class Payment(db.Model):
+  __tablename__='payment'
+  id=db.Column(db.Interger, primary_key=True)
+  state=db.Column(db.Interger, default=0) #状态 0成功 1 失败
+  type=db.Column(db.Interger, default=0) #类型 0 water 1 gas 2 ele
+  amount=db.Column(db.Interger, default=0) #数量
+  fee=db.Column(db.Interger, default=0) #费用
+  room_id=db.Column(db.Interger, db.ForeignKey('rooms.id'), nullable=False)
+  user_id=db.Column(db.Interger, db.ForeignKey('users.id'), nullable=False) # 缴费人id
+  estate_id=db.Column(db.Interger, db.ForeignKey('estates.id'), nullable=False)
