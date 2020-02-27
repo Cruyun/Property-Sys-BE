3fa12ae9d218a56c56a70b12830f0bbff9806600
Cruyun <1045671796@qq.com>
2020-02-27 20:47:28 +0800

    fix api init

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 from flask import Blueprint
 
-main = Blueprint('api', __name__)
+api = Blueprint('api', __name__)
 
 from . import users, estate, payment, rooms, login, announcements, parking, reports
